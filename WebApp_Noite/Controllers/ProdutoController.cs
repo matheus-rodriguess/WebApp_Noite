9ce285075792915cbc505c31fe7d541e8513e62a
yTheuZn <matheus.ro.oli@outlook.com>
2023-03-31 20:35:39 -0300

    Criação de novas telas, cadastrar e lista.

---
+++ WebApp_Noite/Controllers/ProdutoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Noite.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        public IActionResult Lista()
+        {
+            return View();
+        }
+    }
+}
